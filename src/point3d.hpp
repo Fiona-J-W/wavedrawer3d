94afd8787440d2de360d5707b7608bac5ede9a23
Philip Schwartz <philip@philip-P6622.(none)>
2011-01-10 21:05:12 +0100

    class point3d added

---
+++ src/point3d.hpp
@@ -0,0 +1,19 @@
+#ifndef __class_point3d_hpp__
+#define __class_point3d_hpp__
+#include <string>
+
+class point3d{
+	public:
+		point3d(double x=0,double y=0,double z=0);
+		point3d(std::string str);
+		void set(double x,double y,double z);
+		double x;
+		double y;
+		double z;
+};
+
+
+double get_dist_3d(point3d p1,point3d p2); /** calculates the distance between to points **/
+
+
+#endif
