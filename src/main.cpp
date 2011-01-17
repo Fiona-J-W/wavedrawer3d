eee11a08f3db75d0b37bda716560349f15a15107
Florian <florian.weber@piratenpartei-heilbronn.de>
2011-01-17 22:17:23 +0100

    added comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(int argc, char **argv){
 	cout << endl << "wavedrawer3d Alpha" << endl << endl;
-
+	//get the parameters meansing
 	settings S(argc,argv);
-
+	//draw the files
 	draw(S);
 
 	return 0;
