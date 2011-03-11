0bd13e902189cc576d7dcae80b1f27f21ff34d15
Florian_Weber <florian.weber@piratenpartei-heilbronn.de>
2011-03-11 11:58:20 +0100

    deleted unused functions of libpicture

--- src/libpicture.hpp
+++ src/libpicture.hpp
@@ -15,18 +15,8 @@ class picture{
 		color_rgb set_pix(int x,int y,color_rgb color);
 		color_rgb get_pix(int x,int y);
 		color_rgb get_pix_s(int x,int y);
-		void resize(int width,int height);
 		int draw_bmp(string file);
 		int draw_bmp(const char *file);
-		void line(point p1,point p2,color_rgb color=col_black);
-		
-		void rect(point p1,point p2,color_rgb fill=col_black,int stroke=0,
-			  color_rgb stroke_col=col_black);
-		void circle(point m,int r,color_rgb fill=col_black,int stroke=0,
-			  color_rgb stroke_col=col_black);
-		void ellipse(point m,int rx,int ry,color_rgb fill=col_black,int stroke=0,
-			  color_rgb stroke_col=col_black);
-		picture sub_pic(point p1,point p2);
 	protected:
 	//Values:
 		int m_height;
@@ -34,10 +24,6 @@ class picture{
 		vector<vector<color_rgb> > m_data;
 	//Functions:
 		unsigned char splitter(int number, int bytes);
-		int get_dif(int a,int b);
-		int greater(int a,int b);
-		int smaller(int a,int b);
-		double get_dist(point p1,point p2);
 };
 
 
